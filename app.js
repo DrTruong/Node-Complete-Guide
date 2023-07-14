8802a65cfa72a208ab22052be6ad765b5abd2dd0
DrTruong <trtruong1999@gmail.com>
2023-07-14 17:32:50 +0700

    create HTML - CSS - static file

--- app.js
+++ app.js
@@ -1,20 +1,19 @@
-const express = require('express');
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+
+const adminData = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
 const app = express();
 
-app.use('/', (req, res, next) => {
-    console.log('This always run!');
-    next();
-});
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/add-product', (req ,res, next) => {
-    console.log('In another middleware!');
-    res.send('<h1>The "Add Product" Page</h1>');
-});
-
-app.use('/', (req, res, next) => {
-    console.log('In another middleware!');
-    res.send('<h1>Hello from Express!</h1>');
+app.use("/admin", adminData.routes);
+app.use(shopRoutes);
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 
 app.listen(3000);
